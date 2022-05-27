a6e58526eaa5f5b703dff9344c96a9bd7fec16c5
Artem Moshnin <swift.uix@gmail.com>
2022-05-27 22:33:52 +0200

    writing tests for UserService

---
+++ src/test/java/com/example/demo/user/UserServiceTest.java
@@ -0,0 +1,36 @@
+package com.example.demo.user;
+
+
+import static org.mockito.Mockito.when;
+
+import org.assertj.core.util.Sets;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class UserServiceTest {
+    @Autowired
+    private UserService service;
+
+    @MockBean
+    private UserRepository repository;
+
+    @Test
+    public void getList() {
+        when(this.repository.findAll())
+                .thenReturn(
+                        Stream.of(
+                        new User(1L, "alex@gmail.com", "123456", Sets.newHashSet()),
+                        new User(2L, "tom@gmail.com", "123456", Sets.newHashSet())
+                        ).collect(Collectors.toList()));
+        assertEquals(2, this.service.getList().size());
+    }
+}
