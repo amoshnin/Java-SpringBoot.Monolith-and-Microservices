237de6bdf60eec2e237e4ca4f98ac1da25402b03
Artem Moshnin <swift.uix@gmail.com>
2022-05-28 05:51:19 +0200

    methods for posts

--- src/main/java/com/example/demo/post/PostController.java
+++ src/main/java/com/example/demo/post/PostController.java
@@ -1,12 +1,41 @@
 package com.example.demo.post;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping(path="api/posts")
 public class PostController {
     @Autowired
     private PostService postService;
+
+    @GetMapping(path="item/{postId}")
+    public Post getItem(@PathVariable Long postId) {
+        return new Post();
+    }
+
+    @GetMapping(path="list/{userId}")
+    public List<Post> getListByUserId() {
+        List<Post> q = new ArrayList<>();
+        q.add(new Post());
+        return q;
+    }
+
+    @PostMapping(path="item")
+    public void add(Post post) {
+
+    }
+
+    @PutMapping(path="item")
+    public void update(Post post) {
+
+    }
+
+    @DeleteMapping(path="item/{postId}")
+    public void dalete(@PathVariable Long postId) {
+
+    }
 }
