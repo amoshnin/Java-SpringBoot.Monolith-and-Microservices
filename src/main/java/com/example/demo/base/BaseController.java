ef766e07639b945d82e93790fe7168ec410648d6
Artem Moshnin <swift.uix@gmail.com>
2022-05-28 07:21:54 +0200

    added internationalisation

--- src/main/java/com/example/demo/base/BaseController.java
+++ src/main/java/com/example/demo/base/BaseController.java
@@ -1,14 +1,23 @@
 package com.example.demo.base;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Locale;
 
 @RestController
 @RequestMapping(path="/")
 public class BaseController {
+    @Autowired
+    private MessageSource messageSource;
+
     @GetMapping
     public String index() {
-        return "Hello World!";
+        return this.messageSource.getMessage("good.morning", null, "Default message", LocaleContextHolder.getLocale());
     }
 }
